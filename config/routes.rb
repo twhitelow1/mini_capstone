d12afef48f2fa12edfbd075a9e32820e47ac48d5
Todd Whitelow <todd@whitelow.tech>
2020-09-01 18:39:55 -0600

    add while loop for all products

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/all_products_path" => "products#all_products_action"
+    get "/view_product_path" => "products#view_product_action"
   end
 end
