d3f9e052c3c76dcb3d096217c861a77100dbf340
Todd Whitelow <todd@whitelow.tech>
2020-09-01 18:05:38 -0600

    create and populate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Snowboard", price: 25, image_url: "https://todd.whitelow.tech", description: "Whitelow Pro Model board.")
