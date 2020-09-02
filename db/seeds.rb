242e56edd5ac0e1675217d8829c7706dd4489fe1
Todd Whitelow <todd@whitelow.tech>
2020-09-01 18:08:36 -0600

    populate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "Snowboard", price: 25, image_url: "https://todd.whitelow.tech", description: "Whitelow Pro Model board.")
+#Product.create(name: "Snowboard", price: 25, image_url: "https://todd.whitelow.tech", description: "Whitelow Pro Model board.")
+Product.create(name: "Skis", price: 250, image_url: "https://todd.whitelow.tech", description: "Whitelow Pro Model board.")
+Product.create(name: "Burton Snowboard", price: 300, image_url: "https://todd.whitelow.tech", description: "Whitelow Pro Model board.")
+Product.create(name: "K2 Snowboard", price: 450, image_url: "https://todd.whitelow.tech", description: "Whitelow Pro Model board.")
+Product.create(name: "Capita Snowboard", price: 609, image_url: "https://todd.whitelow.tech", description: "Whitelow Pro Model board.")
+Product.create(name: "Solomon Snowboard", price: 650, image_url: "https://todd.whitelow.tech", description: "Whitelow Pro Model board.")
+Product.create(name: "Ride Snowboard", price: 250, image_url: "https://todd.whitelow.tech", description: "Whitelow Pro Model board.")
