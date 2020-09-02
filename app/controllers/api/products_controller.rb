2ef054a726502d55e7a600816a9764163eae9ea6
Todd Whitelow <todd@whitelow.tech>
2020-09-01 18:33:30 -0600

    add product class

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,11 @@
 class Api::ProductsController < ApplicationController
   def all_products_action
+    @products = Product.all
     render "all_products.json.jb"
+  end
+
+  def view_product_action
+    @product = Product.first
+    render "view_product.json.jb"
   end
 end
