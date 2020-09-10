ca965e2c400eb62a06f2cbb2afe75b5530cb7b60
Todd Whitelow <todd@whitelow.tech>
2020-09-09 19:42:44 -0600

    add sort controlls to products controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,16 +1,37 @@
 class Api::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.order("id")
+
+    if params[:search]
+      @products = @products.where("name ILike ?", "%#{params[:search]}%")
+    end
+
+    if params[:discount] == true
+      @products = @products.where("price < ?", 200)
+    end
+
+    if params[:sort] == "price" && params[:sort_order] == "asc"
+      @products = @products.order(price: :asc)
+    elsif params[:sort] == "price" && params[:sort_order] == "desc"
+      @products = @products.order(price: :desc)
+    else
+      @products = @products.order(id: :asc)
+    end
+
     render "index.json.jb"
   end
 
   def update
     @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
     @product.price = params[:price] || @product.price
     @product.image_url = params[:image_url] || @product.image_url
     @product.description = params[:description] || @product.description
-    @product.save
-    render "show.json.jb"
+    if @product.save
+      render "show.json.jb"
+    else
+      render json: { errors: @product.errors.full_messages }, status: 422
+    end
   end
 
   def show
