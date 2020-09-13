1f38bf6495743d006e1fa0792463080211ad7b36
Todd Whitelow <todd@whitelow.tech>
2020-09-13 10:39:04 -0600

    update

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,13 @@ Rails.application.routes.draw do
     post "/products" => "products#create"
     delete "/products/:id" => "products#destroy"
     patch "/products/:id" => "products#update"
+
     post "/images" => "images#create"
     get "/images" => "images#index"
     get "/images/:id" => "images#show"
     delete "/images/:id" => "images#destroy"
     patch "/images/:id" => "images#update"
+
+    post "/users" => "users#create"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_11_100752) do
+ActiveRecord::Schema.define(version: 2020_09_13_162738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 2020_09_11_100752) do
     t.string "email"
     t.string "phone_number"
     t.string "string"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
