c9df72371b640c3b58edbf0832e921d1f5147050
Todd Whitelow <todd@whitelow.tech>
2020-09-14 20:48:19 -0600

    add logic that requires you to be logged in to view an order

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,22 +1,33 @@
 class Api::OrdersController < ApplicationController
   def create
-    @order = Order.new(
-      user_id: params[:user_id],
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      # subtotal: params[:subtotal],
-      # tax: params[:tax],
-      # total: params[:total],
-    )
-    if @order.save
-      render json: { order: @order }
+    if current_user
+      @order = Order.new(
+        user_id: params[:user_id],
+        product_id: params[:product_id],
+        quantity: params[:quantity],
+        # subtotal: params[:subtotal],
+        # tax: params[:tax],
+        # total: params[:total],
+      )
+      if @order.save
+        render json: { order: @order }
+      else
+        render json: { errors: @order.errors.full_messages }, status: 422
+      end
     else
-      render json: { errors: @order.errors.full_messages }, status: 422
+      render json: { message: "You must be logged in to order" }
     end
   end
 
   def show
-    @order = Order.find_by(id: params[:id])
-    render "show.json.jb"
-  end
+    if current_user
+      @order = Order.find_by(id: params[:id])
+      if current_user.id == @order.user_id
+        render "show.json.jb"
+      else
+        render json: {message: "Sorry, it seems this order doesn not belong to you."}
+      end
+      else 
+      render json: {message: "You must be logged in to view this order."}
+    end
 end
