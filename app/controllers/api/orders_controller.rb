df1f5519885d755f2d73149835545cd14f470cb7
Todd Whitelow <todd@whitelow.tech>
2020-09-15 19:47:16 -0600

    Use authenticate_user helper method from app controller on all the actions in the orders controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,45 +1,35 @@
 class Api::OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def create
-    if current_user
-      product = Product.find_by(id: params[:product_id])
-      quantity = params[:quantity].to_f
-      @order = Order.new(
-        user_id: current_user.id,
-        product_id: params[:product_id],
-        quantity: params[:quantity],
-        subtotal: product.price * quantity,git 
-        tax: product.tax * quantity,
-        total: product.total * quantity,
-      )
-      if @order.save
-        render json: { order: @order }
-      else
-        render json: { errors: @order.errors.full_messages }, status: 422
-      end
+    product = Product.find_by(id: params[:product_id])
+    quantity = params[:quantity].to_f
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: product.price * quantity,
+      tax: product.tax * quantity,
+      total: product.total * quantity,
+    )
+    if @order.save
+      render json: { order: @order }
     else
-      render json: { message: "You must be logged in to order" }
+      render json: { errors: @order.errors.full_messages }, status: 422
     end
   end
 
   def show
-    if current_user
-      @order = Order.find_by(id: params[:id])
-      if current_user.id == @order.user_id
-        render "show.json.jb"
-      else
-        render json: { message: "Sorry, it seems this order doesn not belong to you." }
-      end
+    @order = Order.find_by(id: params[:id])
+    if current_user.id == @order.user_id
+      render "show.json.jb"
     else
-      render json: { message: "You must be logged in to view this order." }
+      render json: { message: "Sorry, it seems this order doesn not belong to you." }
     end
   end
 
   def index
-    if current_user
-      @orders = current_user.orders
-      render "index.json.jb"
-    else
-      render json: { message: "You must be logged in to view your orders." }
-    end
+    @orders = current_user.orders
+    render "index.json.jb"
   end
 end
